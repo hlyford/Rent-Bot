c9b926e666c45cac3ea67e1478a984fb9774f576
hlyford11 <honree@EDM-Mac680.local>
2016-01-04 18:16:29 -0800

    rent charge page mostly complete, inlcluding data binding to controller

--- client/app/app.js
+++ client/app/app.js
@@ -6,9 +6,13 @@ app.config(function ($routeProvider, $httpProvider) {
       templateUrl: 'app/charge/charge.html',
       controller: 'ChargeController'
     })   
+    .when('/', {
+      templateUrl: 'app/landing.html',
+      controller: 'ChargeController'
+    })   
     // Your code here
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
-    $httpProvider.interceptors.push('AttachTokens');
+  //  $httpProvider.interceptors.push('AttachTokens');
 })
--- client/app/charge/charge.js
+++ client/app/charge/charge.js
@@ -3,6 +3,18 @@ angular.module('payrent.charge', [])
 .controller('ChargeController', function ($scope, $http) {
 
   $scope.data = {};
+  $scope.data.roommates = [
+    {name: 'Claire', amount: 200, phone: 44},
+    {name: 'Cody', amount: 250, phone: 44},
+    {name: 'Brittany', amount: 7000, phone: 44},
+    {name: 'Cody', amount: 175, phone: 44}
+  ];
+
+  $scope.data.pge = 0;
+  $scope.data.comcast = 70;
+  $scope.total = function (roommateAmt, PgeAmt, comcastAmt) {
+    return roommateAmt + PgeAmt + comcastAmt;
+  }
 
   $scope.sendRent = function() {
 
@@ -10,8 +22,7 @@ angular.module('payrent.charge', [])
       method: 'POST',
       url: '/rentData',
       data: data
-    });
-
+    });    
 
   };
 
