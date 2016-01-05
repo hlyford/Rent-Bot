13525fa9dc5ceec704bee2338f9010a808c8c9d2
hlyford11 <honree@EDM-Mac680.local>
2016-01-05 15:33:22 -0800

    Small heroku mongo addition

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 //connnect to mongo
+var dbURI = process.env.MONGO_URI || 'mongodb://localhost/rent';
 
-mongoose.connect('mongodb://localhost/rent');
+mongoose.connect(dbURI);
 console.log('MongoDB listening...')
 // configure our server with all the middleware and routing
 //require('./config/middleware.js')(app, express);
@@ -20,3 +21,5 @@ console.log('Rent app listening on 8000');
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
+
+//mongodb://heroku_mx2jx9gp:f0peurgnkq6e73r27bkako2td8@ds039145.mongolab.com:39145/heroku_mx2jx9gp
