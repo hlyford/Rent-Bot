99107565268eab63e09d0976b1d2ca82a8543a8f
hlyford11 <honree@EDM-Mac680.local>
2016-01-04 20:08:57 -0800

    get records appearing in db

--- server/charges/chargesController.js
+++ server/charges/chargesController.js
@@ -15,12 +15,24 @@ module.exports = {
       month: body.month,
       pge_total: body.pge,
       comcast_total: body.comcast,      
-    };
-    
+    };    
     createCharge(newCharge);
+       
+  },
+  getCharges: function(req, res, next) {    
+    var records;
+    findAllCharges()
+      .then(function(records) {
+        records = records;
+        console.log('now', records);
+      })  
     
+    res.send(JSON.stringify(records));
+  },
 
-    // organize the data, add the git token, send off
+  sendToVenmo: function(data) {
+    // STILL NEED TO DO ALL THIS
+     // organize the data, add the git token, send off
 
     // request.post({url:'https://api.venmo.com/v1', formData: formData}, function optionalCallback(err, httpResponse, body) {
     //   if (err) {
@@ -29,50 +41,6 @@ module.exports = {
     //   console.log('Upload successful!  Server responded with:', body);
     // });
   },
-  getCharges: function(req, res) {
-    var allCharges = findAllCharges();
-    res.send(allCharges);
-  },
 
-  sendToVenmo: function(data) {
-
-  },
-
-  newLink: function (req, res, next) {
-    var url = req.body.url;
-    if (!util.isValidUrl(url)) {
-      return next(new Error('Not a valid url'));
-    }
-
-    findLink({url: url})
-      .then(function (match) {
-        if (match) {
-          res.send(match);
-        } else {
-          return util.getUrlTitle(url);
-        }
-      })
-      .then(function (title) {
-        if (title) {
-          var newLink = {
-            url: url,
-            visits: 0,
-            base_url: req.headers.origin,
-            title: title
-          };
-          return createLink(newLink);
-        }
-      })
-      .then(function (createdLink) {
-        if (createdLink) {
-          res.json(createdLink);
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });
-  },
-
-  
 
 };
