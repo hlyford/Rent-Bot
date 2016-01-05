8f021e9a4bcb2f8cde72cd574c362681f29ed4c7
hlyford11 <honree@EDM-Mac680.local>
2016-01-05 15:24:30 -0800

    Include conditional db for heroku

--- server/server.js
+++ server/server.js
@@ -13,8 +13,9 @@ console.log('MongoDB listening...')
 
 require('./config/routes.js')(app, express);
 
+var port = process.env.PORT || 8000;
 // start listening to requests on port 8000
-app.listen(8000);
+app.listen(port);
 console.log('Rent app listening on 8000');
 
 // export our app for testing and flexibility, required by index.js
