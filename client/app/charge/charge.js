60c2523cc5042718418a12e8ecb7996a904ead51
hlyford11 <honree@EDM-Mac680.local>
2016-01-05 16:16:30 -0800

    Add super important gif feature

--- client/app/charge/charge.js
+++ client/app/charge/charge.js
@@ -16,15 +16,38 @@ angular.module('payrent.charge', [])
   $scope.data.token;
   $scope.data.month;
 
+  $scope.startMoving = function(img) {
+    var img$ = $(img);
+    var imgWidth = img$.width();
+    var screenWidth = $(window).width();
+    var amount = screenWidth - (parseInt(img$.css("left"), 10) || 0);
+    // if already past right edge, reset to 
+    // just left of left edge
+    if (amount <=0 ) {
+      img$.css("left", -imgWidth);
+      amount = screenWidth + imgWidth;
+    }
+    var moveRate = 300;   // pixels per second to move
+    var time = amount * 1000 / moveRate;
+    img$.stop(true)
+      .animate({left: "+=" + amount}, time, "linear", function() {
+        // when animation finishes, start over
+        $scope.startMoving(this);
+      })
+  };
+
   $scope.showGreenChecks = function(input_field) {
     $('.checks').show();   
     $('.chargeBtn').hide();
     $('.rentHistoryBtn').removeClass('hidden');
+
+    console.log('moving?');
+    var img = $('.mover');
+    img.removeClass('hidden');
+    $scope.startMoving(img);
   }
 
-
-  $scope.sendRent = function() {
-    
+  $scope.sendRent = function() {    
     for (var i = 0; i < $scope.data.roommates.length; i++) {
       $scope.data.roommates[i].total = $scope.data.roommates[i].amount + ($scope.data.pge)/5 + ($scope.data.comcast)/5;
     }
@@ -38,8 +61,8 @@ angular.module('payrent.charge', [])
       console.log('response back');
       setTimeout($scope.showGreenChecks, 800);
     })    
-
   };
+  
 
  
 });
--- client/public/main.js
+++ client/public/main.js
@@ -1,10 +1,15 @@
 $( document ).ready(function() { 
-
+	// readjust if window changes size
+    // $(window).resize(function() {
+    //     $(".mover").each(function() {
+    //         startMoving(this);
+    //     });
+    // });
 	// $(function () {
 	//   $('[data-toggle="tooltip"]').tooltip()
 	// })
 	$('.signupBtn').on('click', function() {
-		$('#signupModal').modal('hide');
+		$('#signupModal').modal('hide');			
 	})
 	// send Twilio test off to backend
 	$('#testTwilio').click(function() {
@@ -15,6 +20,33 @@ $( document ).ready(function() {
 		  console.log('twilio done');
 		});
 	})
+	// $('.chargeBtn').click(function() {
+	// 	console	.log('moving?');
+	// 	var img = $('.mover');
+	// 	img.removeClass('hidden');
+	// 	startMoving(img);
+	// })
+
+	// function startMoving(img) {
+ //    var img$ = $(img);
+ //    var imgWidth = img$.width();
+ //    var screenWidth = $(window).width();
+ //    var amount = screenWidth - (parseInt(img$.css("left"), 10) || 0);
+ //    // if already past right edge, reset to 
+ //    // just left of left edge
+ //    if (amount <=0 ) {
+ //        img$.css("left", -imgWidth);
+ //        amount = screenWidth + imgWidth;
+ //    }
+ //    var moveRate = 300;   // pixels per second to move
+ //    var time = amount * 1000 / moveRate;
+ //    img$.stop(true)
+ //        .animate({left: "+=" + amount}, time, "linear", function() {
+ //            // when animation finishes, start over
+ //            startMoving(this);
+ //        })
+	// };
+        
 
 
 });
